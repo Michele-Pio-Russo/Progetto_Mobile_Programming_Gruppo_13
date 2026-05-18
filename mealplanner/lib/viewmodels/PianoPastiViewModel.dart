faaf4f7005c2808c2fa552a60bb5f0066b0d7fe0
Gerardo205x <g.russo269@studenti.unisa.it>
2026-05-18 12:46:51 +0200

    Inizio viewmodel piano pasti

--- mealplanner/lib/viewmodels/PianoPastiViewModel.dart
+++ mealplanner/lib/viewmodels/PianoPastiViewModel.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/PianoPasti.dart';
 
-class PianoPastiViewModel extends ChangeNotifier{ // con ChangeNotifier, possiamo avvisare la grafica quando i dati cambiano
+class PianoPastiViewModel extends ChangeNotifier{ // Con ChangeNotifier, possiamo avvisare la grafica quando i dati cambiano
   
   // Creiamo e riempiamo la lista che contiene i pasti pianificati (privata, perchè c'è il trattino basso)
   final List<PianoPasti> _pasti = [
@@ -18,27 +18,35 @@ class PianoPastiViewModel extends ChangeNotifier{ // con ChangeNotifier, possiam
   //Metodo get usato dalla View per leggere i pasti
   List<PianoPasti> get pasti => _pasti;
 
+  // Funzione per i requisiti di aggiunta e modifica
   void salvaPasto(String id, String giorno, String tipologia, String nomeRicetta) {
+    // Cerca il pasto tramite l'id
     final index = _pasti.indexWhere((pasto) => pasto.id == id);
 
-    if (index != -1) {
+    // Se esiste, lo sovrascrive (caso di modifica)
+    if (index != -1) { // indexWhere se non trova nulla restituisce -1
       _pasti[index] = PianoPasti(id: id, giorno: giorno, tipologia: tipologia, nomeRicetta: nomeRicetta);
+    // Altrimenti, lo aggiunge al piano pasti (caso di aggiunta)
     } else {
       _pasti.add(PianoPasti(id: id, giorno: giorno, tipologia: tipologia, nomeRicetta: nomeRicetta));
     }
-    
+    // Segnalazione alla View
     notifyListeners();
   }
 
+  // Funzione per il requisito di rimozione
   void eliminaPasto(String id) {
+    // Prendiamo l'indice del pasto da eliminare
     final index = _pasti.indexWhere((pasto) => pasto.id == id);
-    if (index != -1) {
-      _pasti[index] = PianoPasti(
+    if (index != -1) { // Se lo trova:
+      _pasti[index] = PianoPasti( // Salviamo i dati del pasto, ma nomeRicetta è vuoto, così da non visualizzarlo più
         id: _pasti[index].id,
         giorno: _pasti[index].giorno,
         tipologia: _pasti[index].tipologia,
-        nomeRicetta: '-',
+        nomeRicetta: '-', // La view leggerà solo questo
       );
+
+      // Notifichiamo alla View la rimozione
       notifyListeners();
     }
   }
