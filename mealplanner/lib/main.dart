d0f9cae698e01afff48a892ab479d2d2957a5888
Gerardo205x <g.russo269@studenti.unisa.it>
2026-05-30 11:12:53 +0200

    Progetto funzionante tranne ricette

--- mealplanner/lib/main.dart
+++ mealplanner/lib/main.dart
@@ -1,34 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-<<<<<<< Updated upstream
-import 'package:mealplanner/viewmodels/dispensa_viewmodel.dart';
-import 'package:mealplanner/viewmodels/piano_pasti_viewmodel.dart';
-import 'package:mealplanner/viewmodels/ricette_viewmodel.dart';
-import 'package:mealplanner/views/navigazione_view.dart';
-=======
 
-import 'viewmodels/piano_pasti_viewmodel.dart';
 import 'viewmodels/dispensa_viewmodel.dart';
+import 'viewmodels/piano_pasti_viewmodel.dart';
 import 'viewmodels/lista_spesa_viewmodel.dart';
+import 'viewmodels/ricette_viewmodel.dart';
 import 'views/navigazione_view.dart';
->>>>>>> Stashed changes
 
 void main() {
-  // Assicura l'inizializzazione dei binding di Flutter (necessario per l'uso asincrono di SharedPreferences all'avvio)
   WidgetsFlutterBinding.ensureInitialized();
-  
   runApp(
     MultiProvider(
       providers: [
-<<<<<<< Updated upstream
-        ChangeNotifierProvider(create: (context) => GestoreDispensa()), // Il provider per la gestione della dispensa
-        ChangeNotifierProvider(create: (context) => PianoPastiViewModel()), // Il provider per la gestione del piano pasti
-        ChangeNotifierProvider(create: (context) => RicetteViewModel()), // Il provider per la gestione del ricettacolo
-=======
         ChangeNotifierProvider(create: (context) => GestoreDispensa()),
         ChangeNotifierProvider(create: (context) => PianoPastiViewModel()),
         ChangeNotifierProvider(create: (context) => ListaSpesaViewModel()),
->>>>>>> Stashed changes
+        ChangeNotifierProvider(create: (context) => RicetteViewModel()),
       ],
       child: const MyApp(),
     ),
@@ -51,7 +38,7 @@ class MyApp extends StatelessWidget {
           elevation: 0,
           titleTextStyle: TextStyle(
             color: Colors.black,
-            fontSize: 20, 
+            fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
           iconTheme: IconThemeData(color: Colors.black),
